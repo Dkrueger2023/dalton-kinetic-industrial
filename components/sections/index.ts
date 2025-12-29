764ccf72707d8fe7ad4b05176d9d157bee0b7e43
Preston Jenkins <preston@eltonjenkinslaw.com>
2025-12-29 12:26:07 -0600

    chore: add pnpm workspace configuration and remove global styles

--- components/sections/index.ts
+++ components/sections/index.ts
@@ -1,7 +1,6 @@
-export { Navbar } from "./navbar"
-export { HeroSection } from "./hero-section"
-export { AboutSection } from "./about-section"
-export { ServicesSection } from "./services-section"
-export { IndustriesSection } from "./industries-section"
-export { GeographicalReachSection } from "./geographical-reach-section"
-export { Footer } from "./footer"
+export { HeroSection } from "./hero-section";
+export { AboutSection } from "./about-section";
+export { ServicesSection } from "./services-section";
+export { IndustriesSection } from "./industries-section";
+export { GeographicalReachSection } from "./geographical-reach-section";
+export { Footer } from "./footer";
